4c679680cf6de752367d633ee2c84f6778489aae
erixazerro <39600627+erixazerro@users.noreply.github.com>
2021-02-09 13:35:33 -0500

    Create get_profiles.sh

---
+++ Models/DPPI/get_profiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+proteins=$(ls -A1 dataTrain/)
+for p in $proteins
+do
+	psiblast -db swissprot -evalue 0.001 -query Proteins/${p} -out_ascii_pssm dataTrain/${p} -out dataTrain/${p}_output_file -num_iterations 3
+	cat dataTrain/${p} | sed '1,3d' | tac | sed '1,6d' | tac | cut -d' ' -f8- | column -t | tr ' ' '\t' | tr -s '\t' > dataTrain/${p}.mtx
+	rm dataTrain/${p}_output_file
+	rm dataTrain/${p}
+done
