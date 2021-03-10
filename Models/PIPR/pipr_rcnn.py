84b0b24173e62445200d3419e5aa16199c6ebbcc
erixazerro <39600627+erixazerro@users.noreply.github.com>
2021-03-10 14:21:57 -0500

    Add files via upload

--- Models/PIPR/pipr_rcnn.py
+++ Models/PIPR/pipr_rcnn.py
@@ -92,7 +92,7 @@ if args.seq_size is None:
     SEQ_SIZE = 2000
 else:
     SEQ_SIZE = args.seq_size[0]
-if args.loadModel is '':
+if args.loadModel == '':
     pretrained = None
 else:
     pretrained = args.loadModel[0]
@@ -425,10 +425,10 @@ if __name__ == "__main__":
         print('\ntp=%0.0f \nfp=%0.0f \ntn=%0.0f \nfn=%0.0f \n'%(num_true_pos, num_false_pos, num_true_neg, num_false_neg))
         cv += 1
         accuracy = (num_true_pos + num_true_neg) / num_total
-        prec = num_true_pos / (num_true_pos + num_false_pos)
-        recall = num_true_pos / (num_true_pos + num_false_neg)
-        spec = num_true_neg / (num_true_neg + num_false_pos)
-        f1 = 2. * (prec * recall) / (prec + recall)
+        prec = num_true_pos / (num_true_pos + num_false_pos + 1e-06)
+        recall = num_true_pos / (num_true_pos + num_false_neg + 1e-06)
+        spec = num_true_neg / (num_true_neg + num_false_pos + 1e-06)
+        f1 = 2. * (prec * recall) / (prec + recall + 1e-06)
         mcc = (num_true_pos * num_true_neg - num_false_pos * num_false_neg) / (((num_true_pos + num_false_pos) * (num_true_pos + num_false_neg) * (num_false_pos + num_true_neg) * (num_true_neg + num_false_neg)) ** 0.5)
         print('acc=', accuracy, '\nprec=', prec, '\nrecall=', recall, '\nspec=', spec, '\nf1=', f1, '\nmcc=', mcc)
         print('auc_roc=', auc_roc_test, '\nauc_pr=', auc_pr_test)
